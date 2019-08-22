f20b3e4271b726dd48a21dc9e8c0bc876dedd6ed
Ryan M Smith <ryan.michael.smith@gmail.com>
2019-08-22 13:48:32 -0400

    add link to github to navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,11 +3,14 @@ import React, { Component } from "react";
 const NavBar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
-      <a className="navbar-brand" href="#">
+      <div>
         Hello World
         <span className="badge badge-pill badge-secondary m-2">
           {totalCounters}
         </span>
+      </div>
+      <a href="https://github.com/ryanmichaelsmith/counter-app">
+        https://github.com/ryanmichaelsmith/counter-app
       </a>
     </nav>
   );
